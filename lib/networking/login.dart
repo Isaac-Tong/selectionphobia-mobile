0591d8c416079a2d2609f7a970597a25017beb68
Isaac Tong <isaact943@outlook.com>
2020-06-27 02:45:48 +0800

    Changed from localhost to 10.0.0.2 to fix connection issues.

--- lib/networking/login.dart
+++ lib/networking/login.dart
@@ -3,7 +3,7 @@ import 'dart:convert';
 
 Future<http.Response> loginPost(String user, String pass) {
   return http.post(
-    'http://localhost:5000/login',
+    'http://10.0.2.2:5000/login',
     headers: <String, String>{
       'Content-Type': 'application/json; charset=UTF-8',
       'platform': 'mobile',
