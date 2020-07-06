9670e7e9ad31b9bcf336c4072276e0633b935cbe
Isaac Tong <isaact943@outlook.com>
2020-07-07 07:02:33 +0800

    Created UI for vote_screen

--- lib/constants.dart
+++ lib/constants.dart
@@ -11,6 +11,7 @@ const askturquoiseColor = Color(0xff1fbebf);
 const askblueColor = Color(0xff0F78F2);
 const askredColor = Color(0xffF55D4F);
 const askpurpleColor = Color(0xff8832BF);
+const lightLightBlueColor = Color(0xff69DBF5);
 
 //Random Colours
 const pastelorangeColor = Color(0xffefe2c9);
@@ -19,4 +20,6 @@ const pastelblueColor = Color(0xffd1d9ef);
 const pastelpinkColor = Color(0xffeedfe8);
 const greyColor = Color(0xffa5a5a5);
 const gradientColor = Color(0xffF1F5FA);
+
+
 
